f2c786fad11219a51a185871e9ff781892c6d739
jonghyunBaik <pigmammam4@gmail.com>
2023-04-25 12:35:51 +0900

    data update

--- main.js
+++ main.js
@@ -97,38 +97,6 @@ app.listen(8080, () => console.log('Server running'));
 
 const payInfo = new PayInfo("","","","");
 
-// function requestWithBase64 () {
-//   axios
-//     .post(
-//       'https://p9kjr20suo.apigw.ntruss.com/custom/v1/22095/3eeeb774736d9ff549ce7578632cc717d37ed5347c07dd8cf691d03796235ecd/document/receipt', // APIGW Invoke URL
-//       {
-//         images: [
-//           {
-//             format: 'jpeg', // file format
-//             name: 'rec', // image name
-//             data: ''
-//           }
-//         ],
-//         requestId: '22077', // unique string
-//         timestamp: 0,
-//         version: 'V2'
-//       },
-//       {
-//         headers: {
-//           'X-OCR-SECRET': 'Ymh1Q3JuYUl2Q3R6SFVXYVVHcHBuUk12TVlDb1BFaGk=' // Secret Key 
-//         }
-//       }
-//     )
-//     .then(res => {
-//       if (res.status === 200) {
-//         console.log('requestWithBase64 response:', res.data)
-//       }
-//     })
-//     .catch(e => {
-//       console.warn('requestWithBase64 error', e.response)
-//     })
-// }
-
 function requestWithFile (filename) {
   const file = fs.createReadStream(`uploads/${filename}`) // image file object. Example: fs.createReadStream('./example.png')
   
@@ -164,8 +132,9 @@ function requestWithFile (filename) {
       if (res.status === 200) {
         // console.log('requestWithFile response:', res.data)
         const obj = Object.values(Object.values(Object.values(Object.values(Object.values(res.data)[3]))[0])[0])[1]
-        payInfo.name = obj.storeInfo.name.text + ' ' +obj.storeInfo.subName.text
-        payInfo.date = '2023-' + obj.paymentInfo.date.text
+        payInfo.name = obj.storeInfo.name.text + ' ' + obj.storeInfo.subName.text
+        payInfo.date = obj.paymentInfo.date.text
+
         payInfo.price = obj.totalPrice.price.text
         payInfo.address = (Object.values(Object.values(obj)[0])[3])[0].text
         console.log("success")
@@ -176,7 +145,3 @@ function requestWithFile (filename) {
       console.warn('requestWithFile error', e.response)
     })
 }
-
-// requestWithBase64()
-// requestWithFile()
-
